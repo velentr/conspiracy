2a07a70770b51f7e694d0003fbac8704dd546397
Brian Kubisiak <brian@kubisiak.com>
2024-03-18 16:01:14 -0700

    conspiracy: subprocess: Initial revision
    This includes the skeleton of the 'run' function, which mirrors
    python's subprocess.run(), along with the 'call' function (mirroring
    python's subprocess.call()).
    
    Very little functionality is actually implemented here; more will be
    added piece by piece.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

---
+++ conspiracy/subprocess.scm
@@ -0,0 +1,37 @@
+;;; SPDX-FileCopyrightText: 2024 Brian Kubisiak <brian@kubisiak.com>
+;;;
+;;; SPDX-License-Identifier: GPL-3.0-only
+
+(define-module (conspiracy subprocess)
+  #:use-module (srfi srfi-9)
+  #:export (call
+            completed-process?
+            completed-process->args
+            completed-process->returncode
+            run))
+
+(define-record-type <completed-process>
+  (make-completed-process args returncode)
+  completed-process?
+  ;; The argument list that was used to invoke the child process.
+  (args completed-process->args)
+  ;; The exit status of the child process returned by WAITPID. This value can be
+  ;; handled with STATUS:exit-val and related functions. Typically, a return
+  ;; code of 0 indicates success and any other code indicates failure.
+  (returncode completed-process->returncode))
+
+(define* (completed-process* #:key args returncode)
+  "Make a new <COMPLETED-PROCESS> with the given ARGS and RETURNCODE."
+  (make-completed-process args returncode))
+
+(define (run args)
+  "Run the command given by ARGS in another process, wait for it to complete,
+then return a <COMPLETED-PROCESS> representing the result of its execution."
+  (completed-process*
+   #:args args
+   #:returncode (apply system* args)))
+
+(define* (call . args)
+  "Run the command given by ARGS in another process, wait for it to complete,
+then return the returncode that resulted from its execution."
+  (completed-process->returncode (apply run args)))
