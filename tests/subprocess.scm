334fceb323facddd6da0502ccda2cf95f5b71d19
Brian Kubisiak <brian@kubisiak.com>
2024-03-23 06:48:22 -0700

    tests: subprocess: Add additional tests
    Add more exhaustive testing:
    
    - 'call' (without check) will not raise an error on failure
    - Output from 'echo' is captured correctly
    - Setting the environment works correctly
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- tests/subprocess.scm
+++ tests/subprocess.scm
@@ -13,3 +13,17 @@
 (define-test check-subprocess-false
   "Calling 'false' does not return success."
   (assert-error (check-call* "false")))
+
+(define-test nocheck-subprocess-false
+  "Calling 'false' without checking completes successfully."
+  (assert (not (= 0 (call* "false")))))
+
+(define-test check-subprocess-echo
+  "Output from 'echo' is captured."
+  (assert (equal? "foo bar\n" (check-output* "echo" "foo" "bar"))))
+
+(define-test check-subprocess-environment
+  "Subprocess is executed with a clean environment."
+  (assert
+   (equal?
+    "FOO=bar\0" (check-output '("env" "-0") #:environment '(("FOO" . "bar"))))))
