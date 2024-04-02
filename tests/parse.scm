edbacbaa0c663aa7f4b466450f6070f4bceedfe8
Brian Kubisiak <brian@kubisiak.com>
2024-04-02 15:51:38 -0700

    tests: parse: Add syntax stream tests
    Test several properties of syntax streams, including the property that
    the elements are only computed once and syntax errors don't raise
    exceptions until the syntax object is actually requested.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- tests/parse.scm
+++ tests/parse.scm
@@ -4,7 +4,8 @@
 
 (define-module (tests parse)
   #:use-module (conspiracy parse)
-  #:use-module (conspiracy test))
+  #:use-module (conspiracy test)
+  #:use-module (srfi srfi-41))
 
 (define-test test-syntax-children
   "We can extract the children of a syntax object."
@@ -17,3 +18,19 @@
   "Extracting a non-list syntax object is not an error."
   (assert
    (nil? (syntax->children (call-with-input-string "foo" read-syntax)))))
+
+(define-test test-syntax-stream
+  "We can extract syntax tokens from a stream."
+  (let ((strm (string->syntax-stream "foo bar")))
+    (assert (eq? (syntax->datum (stream-car strm)) 'foo))
+    ;; Calling CAR in the stream twice should still return the same datum.
+    (assert (eq? (syntax->datum (stream-car strm)) 'foo))
+    (assert (eq? (syntax->datum (stream-car (stream-cdr strm))) 'bar))
+    (assert (stream-null? (stream-cdr (stream-cdr strm))))))
+
+(define-test test-syntax-stream-parse-error
+  "Malformed code does not immediately cause an error."
+  (let ((strm (string->syntax-stream "foo (")))
+    (assert (eq? (syntax->datum (stream-car strm)) 'foo))
+    (assert (stream? (stream-cdr strm)))
+    (assert-error (stream-car (stream-cdr strm)))))
