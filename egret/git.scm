be4f561965a26ec242d152c641f7706b8fc3e567
Brian Kubisiak <brian@kubisiak.com>
2024-04-10 21:16:54 -0700

    egret: git: Add function for creating bare repositories
    It is unlikely that I'll ever want non-bare git repositories, so
    GIT-INIT will always create a bare repository at the given path if it
    does not already exist.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- egret/git.scm
+++ egret/git.scm
@@ -4,7 +4,8 @@
 
 (define-module (egret git)
   #:use-module (conspiracy subprocess)
-  #:export (read-file))
+  #:export (git-init
+            read-file))
 
 ;; Path to the git executable to use.
 (define %git "git")
@@ -14,3 +15,17 @@
 output to LIMIT bytes."
   (check-output (list %git "-C" path "show" (string-join (list ref file) ":"))
                 #:stdout `(capture ,limit)))
+
+(define (repository? path)
+  "Return #T if PATH is a bare git repository."
+  (and
+   (file-exists? path)
+   (file-is-directory? path)
+   (equal? "true"
+           (string-trim-right
+            (check-output* %git "-C" path "rev-parse" "--is-inside-git-dir")))))
+
+(define (git-init path)
+  "Create a new bare git repository at PATH if it does not already exist."
+  (unless (repository? path)
+    (check-call* %git "init" "--bare" path)))
